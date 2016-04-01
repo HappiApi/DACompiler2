251e6f3df50a6b90c5a88ecd70bc0e39fa611eb7
Daniel Gavrilov <dgavrilov95@gmail.com>
2016-04-01 22:13:40 +0100

    Restructure & optimise unary instructions

--- src/comp207p/main/ConstantFolder.java
+++ src/comp207p/main/ConstantFolder.java
@@ -32,6 +32,13 @@ public class ConstantFolder {
     JavaClass original = null;
     JavaClass optimized = null;
 
+    String reConstPushInstruction   = "(ConstantPushInstruction|LDC|LDC2_W)"; // LDC_W is a subclass of LDC, so we don't need to include it
+    String reUnaryInstruction  = "(DNEG|FNEG|INEG|LNEG)";
+    String reBinaryInstruction = "(DADD|DDIV|DMUL|DREM|DSUB|" +
+                                  "FADD|FDIV|FMUL|FREM|FSUB|" +
+                                  "IADD|IAND|IDIV|IMUL|IOR|IREM|ISHL|ISHR|ISUB|IUSHR|IXOR|" +
+                                  "LADD|LAND|LDIV|LMUL|LOR|LREM|LSHL|LSHR|LSUB|LUSHR|LXOR)";
+
     public ConstantFolder(String classFilePath) {
         try {
             this.parser = new ClassParser(classFilePath);
@@ -67,21 +74,12 @@ public class ConstantFolder {
         // Initialise a method generator with the original method as the baseline
         MethodGen methodGen = new MethodGen(method.getAccessFlags(), method.getReturnType(), method.getArgumentTypes(), null, method.getName(), cgen.getClassName(), instList, cpgen);
 
-        // Use InstructionFinder to search for a pattern of instructions
-        // (in our case, a constant binary expression)
-        String constPush = "(ConstantPushInstruction|LDC|LDC2_W)"; // LDC_W is a subclass of LDC, so we don't need to include it
-        String binaryExprPattern = constPush + " " + constPush + " " + "ArithmeticInstruction";
+        boolean optimizationOccurred = true;
 
-        boolean optimizedLastPass = true;
-
-        while (optimizedLastPass) {
-            optimizedLastPass = false;
-            InstructionFinder f = new InstructionFinder(instList);
-            for (Iterator<?> e = f.search(binaryExprPattern); e.hasNext(); ) {
-                InstructionHandle[] handles = (InstructionHandle[])e.next();
-                boolean optimizedThisPass = this.optimizeConstantBinaryExpr(handles, instList, cpgen);
-                optimizedLastPass = optimizedLastPass || optimizedThisPass;
-            }
+        while (optimizationOccurred) {
+            optimizationOccurred = false;
+            optimizationOccurred = this.optimizeAllUnaryExprs(instList, cpgen) || optimizationOccurred;
+            optimizationOccurred = this.optimizeAllBinaryExprs(instList, cpgen) || optimizationOccurred;
         }
 
         // setPositions(true) checks whether jump handles
@@ -96,6 +94,90 @@ public class ConstantFolder {
         Method newMethod = methodGen.getMethod();
         // replace the method in the original class
         cgen.replaceMethod(method, newMethod);
+    }
+
+    public boolean optimizeAllUnaryExprs(InstructionList instList, ConstantPoolGen cpgen) {
+
+        // Use InstructionFinder to search for a pattern of instructions
+        // (in our case, a constant binary expression)
+        String pattern = reConstPushInstruction + " " + reUnaryInstruction;
+
+        boolean optimizedLastPass = true;
+        boolean somethingWasOptimized = false;
+
+        while (optimizedLastPass) {
+            optimizedLastPass = false;
+            InstructionFinder f = new InstructionFinder(instList);
+            for (Iterator<?> e = f.search(pattern); e.hasNext(); ) {
+                InstructionHandle[] handles = (InstructionHandle[])e.next();
+                boolean optimizedThisPass = this.optimizeConstantUnaryExpr(handles, instList, cpgen);
+                optimizedLastPass = optimizedLastPass || optimizedThisPass;
+                somethingWasOptimized = somethingWasOptimized || optimizedThisPass;
+            }
+        }
+
+        return somethingWasOptimized;
+    }
+
+    public boolean optimizeConstantUnaryExpr(InstructionHandle[] handles, InstructionList instList, ConstantPoolGen cpgen) {
+
+        InstructionHandle operand = handles[0];
+        InstructionHandle operator = handles[1];
+
+        String opName = operator.getInstruction().getName();
+        Object value = getValue(operand.getInstruction(), cpgen);
+
+        // Integer operations
+
+        if (opName.equals("ineg")) {
+            instList.insert(operand, new PUSH(cpgen, -(int)value));
+        } else if (opName.equals("lneg")) {
+            instList.insert(operand, new PUSH(cpgen, -(long)value));
+        } else if (opName.equals("fneg")) {
+            instList.insert(operand, new PUSH(cpgen, -(float)value));
+        } else if (opName.equals("dneg")) {
+            instList.insert(operand, new PUSH(cpgen, -(double)value));
+        } else {
+            // reached when instruction is not handled, e.g. bitwise operators or shifts.
+            System.out.println("Couldn't optimise: " + opName);
+            // return is to prevent deleting instructions, since nothing has been added.
+            return false;
+        }
+
+        // Delete the 3 instructions making up the expression
+        try {
+            instList.delete(operand);
+            instList.delete(operator);
+        } catch (TargetLostException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Optimised: " + opName);
+
+        return true;
+    }
+
+    public boolean optimizeAllBinaryExprs(InstructionList instList, ConstantPoolGen cpgen) {
+
+        // Use InstructionFinder to search for a pattern of instructions
+        // (in our case, a constant binary expression)
+        String pattern = reConstPushInstruction + " " + reConstPushInstruction + " " + reBinaryInstruction;
+
+        boolean optimizedLastPass = true;
+        boolean somethingWasOptimized = false;
+
+        while (optimizedLastPass) {
+            optimizedLastPass = false;
+            InstructionFinder f = new InstructionFinder(instList);
+            for (Iterator<?> e = f.search(pattern); e.hasNext(); ) {
+                InstructionHandle[] handles = (InstructionHandle[])e.next();
+                boolean optimizedThisPass = this.optimizeConstantBinaryExpr(handles, instList, cpgen);
+                optimizedLastPass = optimizedLastPass || optimizedThisPass;
+                somethingWasOptimized = somethingWasOptimized || optimizedThisPass;
+            }
+        }
+
+        return somethingWasOptimized;
     }
 
     // Converts binary arithmetic operation to a single constant.
